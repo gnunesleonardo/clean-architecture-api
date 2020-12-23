93371fd780f498ff38fd83b949f5096350f384e7
Leonardo G. Nunes <gnunes.leonardo@gmail.com>
2020-12-23 08:15:34 -0300

    test: verrify name while creating user

--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidEmailError } from '../../src/entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../src/entities/errors/invalid-name-error'
 import { User } from '../../src/entities/user'
 import { left } from '../../src/shared/either'
 
@@ -7,5 +8,17 @@ describe('User domain class', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name (too few chars)', () => {
+    const invalidName = 'L          '
+    const error = User.create({ name: invalidName, email: 'any@email.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  test('should not create user with invalid name (too many chars)', () => {
+    const invalidName = 'L'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@email.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
