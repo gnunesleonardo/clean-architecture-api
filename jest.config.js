4bf802a1b30c0c1f4f46144b63ab761627a44bb8
Leonardo G. Nunes <gnunes.leonardo@gmail.com>
2021-01-14 19:27:23 -0300

    chore: improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/tests/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/tests/**/*.ts',
+    '!**/test/**',
+    '!**/config/**'
+  ],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
