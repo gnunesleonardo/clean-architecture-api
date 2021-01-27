d25e9250322de8d6c5b3b2e445d0479b48f36190
Leonardo G. Nunes <gnunes.leonardo@gmail.com>
2021-01-26 21:44:14 -0300

    refactor: simplify imports

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -1,5 +1,5 @@
-import { UserRepository } from '../ports/user-repository'
-import { UserData } from '../../../entities/user-data'
+import { UserRepository } from '../ports/'
+import { UserData } from '../../../entities/'
 
 export class InMemoryUserRepository implements UserRepository {
   private repository: UserData[]
