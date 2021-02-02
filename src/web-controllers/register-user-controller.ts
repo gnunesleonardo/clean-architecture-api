f73fb89816f15940d039b2eeca0321e43d7eaef8
Leonardo G. Nunes <gnunes.leonardo@gmail.com>
2021-02-02 00:02:17 -0300

    feat: web controller should return 500 when server raises

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,40 +1,44 @@
 import { UserData } from '../entities'
-import { RegisterUserOnMailingList } from '../usecases/register-user-on-mailing-list'
+import { UseCase } from '../usecases/ports'
 import { MissingParamError } from './errors'
 import { HttpRequest, HttpResponse } from './ports'
-import { badRequest, created } from './util/http-helper'
+import { badRequest, created, serverError } from './util/http-helper'
 
 export class RegisterUserController {
-  private readonly usecase: RegisterUserOnMailingList
+  private readonly usecase: UseCase
 
-  constructor (usecase: RegisterUserOnMailingList) {
+  constructor (usecase: UseCase) {
     this.usecase = usecase
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.name || !request.body.email) {
-      let missingParam: string = ''
+    try {
+      if (!request.body.name || !request.body.email) {
+        let missingParam: string = ''
 
-      if (!request.body.name && !request.body.email) {
-        missingParam = 'name and email'
-      } else if (!request.body.name) {
-        missingParam = 'name'
-      } else {
-        missingParam = 'email'
+        if (!request.body.name && !request.body.email) {
+          missingParam = 'name and email'
+        } else if (!request.body.name) {
+          missingParam = 'name'
+        } else {
+          missingParam = 'email'
+        }
+
+        return badRequest(new MissingParamError(missingParam))
       }
 
-      return badRequest(new MissingParamError(missingParam))
-    }
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
 
-    const userData: UserData = request.body
-    const response = await this.usecase.registerUserOnMailingList(userData)
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
-
-    if (response.isRight()) {
-      return created(response.value)
+      if (response.isRight()) {
+        return created(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
