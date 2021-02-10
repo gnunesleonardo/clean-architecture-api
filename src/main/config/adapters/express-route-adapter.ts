9c9a09b100765f0dba9fab7d5a2c98a0265a4d2c
Leonardo G. Nunes <gnunes.leonardo@gmail.com>
2021-02-10 19:56:14 -0300

    feat: express route adapter

---
+++ src/main/config/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+import { HttpRequest } from '../../../web-controllers/ports'
+import { RegisterUserController } from '../../../web-controllers/register-user-controller'
+
+export const adaptRoute = (controller: RegisterUserController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
